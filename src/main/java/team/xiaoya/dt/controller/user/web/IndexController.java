07e18eeb4e75a416830663e77975b5871f9afe0a
wzc <wangzhichaomin@163.com>
2015-11-17 14:55:46 +0800

    xiaoyadati

---
+++ src/main/java/team/xiaoya/dt/controller/user/web/IndexController.java
@@ -0,0 +1,29 @@
+package team.xiaoya.dt.controller.user.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author wangzhichao
+ *
+ */
+
+
+@Controller("/")
+public class IndexController {
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	@RequestMapping
+	public ModelAndView index() {
+		
+		
+		return new ModelAndV
+	}
+
+}
